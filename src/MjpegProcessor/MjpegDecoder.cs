4c84cf9e9abaff481554c5774d7d3115dae44438
Jonas Follesø <jonas@follesoe.no>
2016-09-07 13:00:34 +0200

    Fix indentation after copying code from original repo

--- src/MjpegProcessor/MjpegDecoder.cs
+++ src/MjpegProcessor/MjpegDecoder.cs
@@ -12,20 +12,20 @@ namespace MjpegProcessor
         private readonly byte[] JpegHeader = { 0xff, 0xd8 };
 
         // pull down 1024 bytes at a time
-		private const int ChunkSize = 1024;
+        private const int ChunkSize = 1024;
 
-		// used to cancel reading the stream
-		private bool _streamActive;
+        // used to cancel reading the stream
+        private bool _streamActive;
 
-		// current encoded JPEG image
-		public byte[] CurrentFrame { get; private set; }
+        // current encoded JPEG image
+        public byte[] CurrentFrame { get; private set; }
 
         // used to marshal back to UI thread
-		private SynchronizationContext _context;
+        private SynchronizationContext _context;
 
-		// event to get the buffer above handed to you
-		public event EventHandler<FrameReadyEventArgs> FrameReady;
-		public event EventHandler<ErrorEventArgs> Error;
+        // event to get the buffer above handed to you
+        public event EventHandler<FrameReadyEventArgs> FrameReady;
+        public event EventHandler<ErrorEventArgs> Error;
 
         public MjpegDecoder()
         {
@@ -37,137 +37,137 @@ namespace MjpegProcessor
             var request = (HttpWebRequest)WebRequest.Create(uri);
             
             // asynchronously get a response
-			request.BeginGetResponse(OnGetResponse, request);
+            request.BeginGetResponse(OnGetResponse, request);
         }
 
-		public void StopStream()
-		{
-			_streamActive = false;
-		}
+        public void StopStream()
+        {
+            _streamActive = false;
+        }
 
         private void OnGetResponse(IAsyncResult asyncResult)
-		{
-			byte[] imageBuffer = new byte[1024 * 1024];
+        {
+            byte[] imageBuffer = new byte[1024 * 1024];
 
             // get the response
-			var req = (HttpWebRequest)asyncResult.AsyncState;
+            var req = (HttpWebRequest)asyncResult.AsyncState;
 
             try
-			{
-				HttpWebResponse resp = (HttpWebResponse)req.EndGetResponse(asyncResult);
+            {
+                HttpWebResponse resp = (HttpWebResponse)req.EndGetResponse(asyncResult);
 
-				// find our magic boundary value
-				string contentType = resp.Headers["Content-Type"];
-				if(!string.IsNullOrEmpty(contentType) && !contentType.Contains("="))
-					throw new Exception("Invalid content-type header.  The camera is likely not returning a proper MJPEG stream.");
+                // find our magic boundary value
+                string contentType = resp.Headers["Content-Type"];
+                if(!string.IsNullOrEmpty(contentType) && !contentType.Contains("="))
+                    throw new Exception("Invalid content-type header.  The camera is likely not returning a proper MJPEG stream.");
 
-				string boundary = resp.Headers["Content-Type"].Split('=')[1].Replace("\"", "");
-				byte[] boundaryBytes = Encoding.UTF8.GetBytes(boundary.StartsWith("--") ? boundary : "--" + boundary);
+                string boundary = resp.Headers["Content-Type"].Split('=')[1].Replace("\"", "");
+                byte[] boundaryBytes = Encoding.UTF8.GetBytes(boundary.StartsWith("--") ? boundary : "--" + boundary);
 
                 Stream s = resp.GetResponseStream();
-				var br = new BinaryReader(s);
+                var br = new BinaryReader(s);
 
-				_streamActive = true;
+                _streamActive = true;
 
-				byte[] buff = br.ReadBytes(ChunkSize);
+                byte[] buff = br.ReadBytes(ChunkSize);
 
-				while (_streamActive)
-				{
+                while (_streamActive)
+                {
                     // find the JPEG header
-					int imageStart = buff.Find(JpegHeader);
+                    int imageStart = buff.Find(JpegHeader);
 
-					if(imageStart != -1)
-					{
+                    if(imageStart != -1)
+                    {
                         // copy the start of the JPEG image to the imageBuffer
-						int size = buff.Length - imageStart;
-						Array.Copy(buff, imageStart, imageBuffer, 0, size);
+                        int size = buff.Length - imageStart;
+                        Array.Copy(buff, imageStart, imageBuffer, 0, size);
 
-						while(true)
-						{
+                        while(true)
+                        {
                             buff = br.ReadBytes(ChunkSize);
 
-							// find the boundary text
-							int imageEnd = buff.Find(boundaryBytes);
-							if(imageEnd != -1)
-							{
+                            // find the boundary text
+                            int imageEnd = buff.Find(boundaryBytes);
+                            if(imageEnd != -1)
+                            {
                                 // copy the remainder of the JPEG to the imageBuffer
-								Array.Copy(buff, 0, imageBuffer, size, imageEnd);
-								size += imageEnd;
+                                Array.Copy(buff, 0, imageBuffer, size, imageEnd);
+                                size += imageEnd;
 
-								byte[] frame = new byte[size];
-								Array.Copy(imageBuffer, 0, frame, 0, size);
+                                byte[] frame = new byte[size];
+                                Array.Copy(imageBuffer, 0, frame, 0, size);
 
-								ProcessFrame(frame);
+                                ProcessFrame(frame);
 
-								// copy the leftover data to the start
-								Array.Copy(buff, imageEnd, buff, 0, buff.Length - imageEnd);
+                                // copy the leftover data to the start
+                                Array.Copy(buff, imageEnd, buff, 0, buff.Length - imageEnd);
 
-								// fill the remainder of the buffer with new data and start over
-								byte[] temp = br.ReadBytes(imageEnd);
+                                // fill the remainder of the buffer with new data and start over
+                                byte[] temp = br.ReadBytes(imageEnd);
 
-								Array.Copy(temp, 0, buff, buff.Length - imageEnd, temp.Length);
-								break;
+                                Array.Copy(temp, 0, buff, buff.Length - imageEnd, temp.Length);
+                                break;
                             }
 
                             // copy all of the data to the imageBuffer
-							Array.Copy(buff, 0, imageBuffer, size, buff.Length);
-							size += buff.Length;
+                            Array.Copy(buff, 0, imageBuffer, size, buff.Length);
+                            size += buff.Length;
                         }
                     }
                 }
 
             }
             catch(Exception ex)
-			{
+            {
                 if(Error != null)
-					_context.Post(delegate { Error(this, new ErrorEventArgs(ex.Message)); }, null);
+                    _context.Post(delegate { Error(this, new ErrorEventArgs(ex.Message)); }, null);
             }
         }
 
         private void ProcessFrame(byte[] frame)
-		{
-			CurrentFrame = frame;
+        {
+            CurrentFrame = frame;
             _context.Post(delegate
-				{
-					// tell whoever's listening that we have a frame to draw
-					if(FrameReady != null)
-						FrameReady(this, new FrameReadyEventArgs(CurrentFrame));
-				}, null);
+                {
+                    // tell whoever's listening that we have a frame to draw
+                    if(FrameReady != null)
+                        FrameReady(this, new FrameReadyEventArgs(CurrentFrame));
+                }, null);
         }                                
     }
 
     internal static class Extensions
-	{
-		public static int Find(this byte[] buff, byte[] search)
-		{
-			// enumerate the buffer but don't overstep the bounds
-			for(int start = 0; start < buff.Length - search.Length; start++)
-			{
-				// we found the first character
-				if(buff[start] == search[0])
-				{
-					int next;
+    {
+        public static int Find(this byte[] buff, byte[] search)
+        {
+            // enumerate the buffer but don't overstep the bounds
+            for(int start = 0; start < buff.Length - search.Length; start++)
+            {
+                // we found the first character
+                if(buff[start] == search[0])
+                {
+                    int next;
 
-					// traverse the rest of the bytes
-					for(next = 1; next < search.Length; next++)
-					{
-						// if we don't match, bail
-						if(buff[start+next] != search[next])
-							break;
-					}
+                    // traverse the rest of the bytes
+                    for(next = 1; next < search.Length; next++)
+                    {
+                        // if we don't match, bail
+                        if(buff[start+next] != search[next])
+                            break;
+                    }
 
-					if(next == search.Length)
-						return start;
-				}
-			}
-			// not found
-			return -1;	
-		}
-	}
+                    if(next == search.Length)
+                        return start;
+                }
+            }
+            // not found
+            return -1;	
+        }
+    }
 
     public class FrameReadyEventArgs : EventArgs
-	{
-		public byte[] FrameBuffer { get; }
+    {
+        public byte[] FrameBuffer { get; }
 
         public FrameReadyEventArgs(byte[] buffer)
         {
